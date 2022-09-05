ed74351d9e27cde17dbefd9d3522ca9aa147c35e
kaareskokebok <109585723+kaareskokebok@users.noreply.github.com>
2022-09-05 12:03:49 +0200

    Add files via upload

---
+++ kap1/1_opg4A.py
@@ -0,0 +1,5 @@
+from math import*
+print((10*3)-7)
+print((3**5)+(2**10))
+print((20-(8/2))*2)
+print(10**(46-(7**2)))
---
+++ kap1/1_opg5A.py
@@ -0,0 +1,5 @@
+from math import*
+print(sqrt(3))
+print(3**0.5)
+print(sqrt(6))
+print(6**0.5)
